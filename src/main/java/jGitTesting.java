6581203f9ed6966d6381b1bef4baa9037034ecb0
tommy <tglynn2@oswego.edu>
2024-10-17 14:25:49 -0400

    Demo

--- src/main/java/jGitTesting.java
+++ src/main/java/jGitTesting.java
@@ -15,6 +15,7 @@ public class jGitTesting {
     public static void main(String[] args) {
         String repoPath = "C:\\Users\\tommy\\IdeaProjects\\gitTesting";
         String filePath = "C:\\cygwin64\\home\\tommy\\322hw\\sortHw.py";
+        String filePath2 = "C:\\cygwin64\\home\\tommy\\322hw\\sortHwCSV.py";
         String configFilePath = "config.properties";
 
         try {
@@ -22,7 +23,8 @@ public class jGitTesting {
             String username = props.getProperty("username");
             String password = props.getProperty("password");
 
-            uploadFile(repoPath, filePath, username, password);
+            uploadFile(repoPath, filePath2, username, password);
+            deleteFile(repoPath, filePath, username, password);
         } catch (IOException | GitAPIException e) {
             e.printStackTrace();
         }
@@ -41,17 +43,32 @@ public class jGitTesting {
         Git git = Git.open(repoDir);
         Path sourceFile = Path.of(filePath);
         Path destinationFile = Path.of(repoDir.getAbsolutePath(), sourceFile.getFileName().toString());
-
-        // Copy the file into the repository
         Files.copy(sourceFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
-
-        // Add, commit, and push the file
         git.add().addFilepattern(sourceFile.getFileName().toString()).call();
         git.commit().setMessage("Added " + sourceFile.getFileName()).call();
         git.push()
                 .setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
                 .call();
-
         System.out.println("File uploaded to Git repo!");
     }
+
+    public static void deleteFile(String repoPath, String filePath, String username, String password) throws IOException, GitAPIException {
+        File repoDir = new File(repoPath);
+        Git git = Git.open(repoDir);
+        Path sourceFile = Path.of(filePath);
+        Path fileToDelete = Path.of(repoDir.getAbsolutePath(), sourceFile.getFileName().toString());
+
+        // Delete the file from the repository directory
+        Files.deleteIfExists(fileToDelete);
+
+        // Stage the deleted file in the Git index
+        git.add().addFilepattern(sourceFile.getFileName().toString()).call();
+        git.commit().setMessage("Deleted " + sourceFile.getFileName()).call();
+        git.push()
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
+                .call();
+
+        System.out.println("File deleted from Git repo!");
+    }
+
 }
